faffceac7854a5fe0935764b60e1b90ef5409893
Genevieve016 <gennyorons@gmail.com>
2023-04-26 14:06:47 -0700

    Task1 Check7

--- shell_01.c
+++ shell_01.c
@@ -1,15 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-#define BUFFER_SIZE 1024
+#include "shell.h"
 
 /**
  * main - main function
  *
- * Return: 0
+ * Return: exit
  *
  */
 
