37d1fda1871ce91d5741af9fd8f3e80ffb0dd55c
Genevieve016 <gennyorons@gmail.com>
2023-04-26 17:03:47 -0700

    Task 1 revised

---
+++ is_delim.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * is_delim - checks if character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+	{
+		if (*delim == c)
+			return (1);
+		delim++;
+	}
+	return (0);
+}
