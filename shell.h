568df4f8c23530380fbc53ba2d68cff8a12075e8
genevieve016 <gennyorons@gmail.com>
2023-04-27 14:24:58 -0700

    SS Check

--- shell.h
+++ shell.h
@@ -1,6 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+#define MAX_ENV_VARS 100
+
 /**
  * struct data_shell_s - name of struct
  * struct data_shell - data structure for the shell
